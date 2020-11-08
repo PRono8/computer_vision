c34a41712ca06d9431d1c18b4c331ecec8a119fd
José Rodríguez <peperono8@gmail.com>
2020-11-08 17:15:08 +0100

    global Segment

--- Segmentacion/signatura_isa.m
+++ Segmentacion/signatura_isa.m
@@ -10,7 +10,9 @@ distancia = sqrt((x-mx).^2+(y-my).^2);  % Distancia al centroide de los puntos d
 angulo = atan2(y-my,x-mx);              % Ángulo del punto del contorno con respecto al centroide
 [angulo,orden] = sort(angulo);          % Ordenar distancia según el ángulo
 s = distancia(orden);                   % Signatura
+s = s-min(s);                              
 s = s/max(s);                           % Signatura normalizada
+
 
 sig = [angulo+pi,s];
 
