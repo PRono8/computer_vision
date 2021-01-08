fe6fe5039059e55764c18f9fe569c192b1ebd3b5
Mario Llamas <mariollamas@alumnos.upm.es>
2021-01-08 21:42:00 +0100

    Detecta orientación en Num_identif
    La función test sirve para comprobar el funcionamiento de Num_identification

--- Clasificacion_Num/Num_Identification.m
+++ Clasificacion_Num/Num_Identification.m
@@ -1,5 +1,25 @@
-function [n] = Num_Identification(im, mostrar)
-%%%%%% Clasificación de Números %%%%%%%
+function [n,rotado] = Num_Identification(im, rotar, mostrar)
+
+%%% VARIABLES DE LA FUNCIÓN:
+    % rotado: Te dice si detecta el número rotado
+    %         0 -> No lo sé
+    %         1 -> Correcto
+    %         2 -> 90º
+    %         3 -> 180º = invertido
+
+    % rotar: Si es necesario comprobar la rot del núm
+    %         0 -> No
+    %         1 -> Si
+    %         2 -> Ya rotado 90º (puede estar invertido)
+
+    % Mostrar: si se deben mostrar los resultados (plots)
+    %         0 -> No
+    %         1 -> Si
+    
+    rotado = 0;                             % Inicializamos a cero y sobreescribiremos
+    n = 0;
+
+%%%%%% PROPERTIES OBTENCION %%%%%%%
 
     imh = histeq(im);                       % Mejora el contraste mediante la ecualización del histograma
     imbw = im2bw(imh,0.2);                  % Convertir imagen a imagen binaria, basada en umbral
@@ -31,9 +51,10 @@ function [n] = Num_Identification(im, mostrar)
 
 
     X_pf = Cen(1)-Pfc(1);                   % Posición relativa en Y del primer punto final respecto al centroide
-    v = [Euler, Pf, Area, X_pf ]
+    v = [Euler, Pf, Area, X_pf ];
 
-    n = 0;
+    %%%%%% Clasificación de Números %%%%%%%
+    
     switch (v(1))                           % Separa (8), (0,6,9,4) y (1,2,3,5,7)
      case (-1)                              % Dos huecos
         n = 8;
@@ -76,12 +97,50 @@ function [n] = Num_Identification(im, mostrar)
       subplot(223); imshow(im2);        title('cerrada')
       subplot(224); imshow(esqueleto);  title('esqueleto')
       
-        figure;
-      imshow(double(im).*not(esqueleto)+esqueleto*256,[gray(255);0 1 0]);
+        figure;     imshow(ob1);        title('endpoints')
+        hold on;    plot(Cen(1),Cen(2),'*r')
+        hold on;    plot(Pfc(1),Pfc(2),'*b')
+        hold on;    text(10,20,num2str(n),'Color','r','BackgroundColor','g');
       
         figure; 
       imshow(im); hold on
       text(10,20,num2str(n),'Color','r','BackgroundColor','g');
+    end
+    
+    if rotar == 1
+        BBox = regionprops(esqueleto,'BoundingBox');
+        ancho = BBox.BoundingBox(3);
+        alto  = BBox.BoundingBox(4); 
+        
+        if ancho > alto
+            rotado = 2;
+        else
+            rotar  = 2;      % Para comprobar si está invertido
+        end
+    end
+    
+    if rotar == 2 && (n == 1 || n == 2 || n == 7)
+        switch n
+            case 1
+                if X_pf < -10
+                    rotado = 3;
+                else
+                    rotado = 1;
+                end
+            case 2
+                Y_pf = Cen(2)-Pfc(2);
+                if Y_pf > -50
+                    rotado = 3;
+                else
+                    rotado = 1;
+                end
+            case 7
+                if X_pf < 0
+                    rotado = 3;
+                else
+                    rotado = 1;  
+                end
+        end
     end
 end
 
---
+++ Clasificacion_Num/Test.m
@@ -0,0 +1,47 @@
+for i  = 1:9
+    for k = i:10:i+30
+        
+        im = imread(strcat('Num_', num2str(k),'.jpeg'));
+        
+        im_i = imrotate(im,180);
+        im_r = imrotate(im,90);
+        im_r2 = imrotate(im,-90);
+        
+        [n,rotado] = Num_Identification(im, 1, 0);
+        [n_i,rotado_i] = Num_Identification(im_i, 2, 0);
+        [n_r,rotado_r] = Num_Identification(im_r, 1, 0);
+        [n_r2,rotado_r2] = Num_Identification(im_r2, 1, 0);
+        
+%         numero = k
+%         numero_correcto = n
+%         rotacion_1 = rotado
+%         numero_inverso  = n_i
+%         rotacion_3 = rotado_i
+%         numero_tumbado  = n_r
+%         rotacion_2 = rotado_r
+%         numero_tumbado2 = n_r2
+%         rotacion_2 = rotado_r2
+        
+        [k 0 ;n rotado ;n_r rotado_r ;n_r2 rotado_r2; n_i rotado_i ]
+        
+        pause;
+        clc;
+    end
+    pause;
+end
+
+% Comprobar orientación:
+% 1 : X_pf > un pequeño límite
+% 2 : Dist del pfc /puede salir como 5
+% 3 : Detecta 5
+% 4 : Detecta 4
+% 5 : Detecta 5, pero puede ser 3 al revés
+% 6 : Detecta 9
+% 7 : X_pf + ó -
+% 8 : Detecta 8
+% 9 : Detecta 6
+
+% Nos sirven: 1 2 7
+% Seguros: 1 2 3 4 7 8
+% Dudosos: 5 6 9
+% *Tumbado no se reconoce ninguno, pero es solo una iteración
